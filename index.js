6a15e3ee46411bc304785572ad83e522182780dd
cjr-- <cjr@hotmail.co.uk>
2018-03-09 16:23:07 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+define(module, function(exports, require) {
+
+  var qp = require('qp-utility');
+  var pool = require('./lib/connection_pool');
+  var connection = require('./lib/connection');
+  var notifier = require('./lib/event_notifier');
+
+  exports({
+
+    client: function(options) {
+      return pool.create_client(options);
+    },
+
+    notifier: function(options) {
+      return notifier.create(options);
+    },
+
+    open: function(options) {
+      return pool.open(options);
+    },
+
+    connect: function(handler) {
+      pool.connect((error, db_connection, close_connection) => {
+        if (error) handler(error);
+        else handler(null, connection.create({ connection: db_connection }), close_connection);
+      });
+    },
+
+    close: function(done) {
+      pool.close(done);
+    }
+
+  });
+
+});
